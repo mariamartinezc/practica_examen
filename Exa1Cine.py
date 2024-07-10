82cc2c12466afd7d1d497ebb5afe4b29b347381f
mariamartinezc <mp.martinez@duocuc.cl>
2024-07-10 00:50:53 -0400

    Exa1Cine.py

--- Exa1Cine.py
+++ Exa1Cine.py
@@ -29,87 +29,88 @@ def actualizarrecaudacionconcierto(tipoentrada, costoentrada):
 # Opción 1: Comprar entrada
 def comprar_entrada():
     try:
-        #solicitar tipo de entrada
-        print("-------------------------------------------------")
-        print("|      Bienvenido a la compra de entradas       |")
-        print("-------------------------------------------------")
-        print("|Los precios de las entradas son los siguientes:|")
-        print("|1.- PLATINUM, $120.000. (Asientos del 1 al 20).|")
-        print("|2.- GOLD, $80.000. (Asientos del 21 al 50).    |")
-        print("|3.- SILVER, $50.000. (Asientos del 51 al 100). |")
-        print("-------------------------------------------------")
-        tipodeentrada =int(input("Ingrese el tipo de entrada que desea (1-3) :"))
-        if tipodeentrada < 1 or tipodeentrada > 2: 
-            print("El tipo de entrada ingresado no es válido")
-            return
-        else:
-            if tipodeentrada == 1:
-                precio = 120000
-                rangoasiento = (1,20) 
-                tipoentrada = "PLATINUM"
-            elif tipodeentrada == 2:
-                precio = 80000
-                rangoasiento = (21,50) 
-                tipoentrada = "GOLD"
-            elif tipodeentrada == 3:
-                precio = 80000
-                rangoasiento = (51,100) 
-                tipoentrada = "SILVER"
-            else:
-                print("El tipo de entrada no es válido")
+            #solicitar tipo de entrada
+            print("-------------------------------------------------")
+            print("|      Bienvenido a la compra de entradas       |")
+            print("-------------------------------------------------")
+            print("|Los precios de las entradas son los siguientes:|")
+            print("|1.- PLATINUM, $120.000. (Asientos del 1 al 20).|")
+            print("|2.- GOLD, $80.000. (Asientos del 21 al 50).    |")
+            print("|3.- SILVER, $50.000. (Asientos del 51 al 100). |")
+            print("-------------------------------------------------")
+            tipodeentrada =int(input("Ingrese el tipo de entrada que desea (1-3) :"))
+            if tipodeentrada < 1 or tipodeentrada > 2: 
+                print("El tipo de entrada ingresado no es válido")
                 return
-        # Solicita la cantidad de entradas que desea comprar
-        cantidadentradas = int(input("Ingrese la cantidad de entradas que desea comprar (1-3) : "))
-        if cantidadentradas < 0 or cantidadentradas > 3:
-            print("La cantidad debe ser mayor que 0 y menor o igual a 3")
-            return
-        # Solicita el asiento que desea comprar
-        for i in range(cantidadentradas):
-            print("-----------------------------------------")
-            print("|              ESCENARIO                |")
-            print("-----------------------------------------")
-            print(mostrar_ubicaciones_disponibles())
-            print(f"Tipo de entrada seleccionada : {tipoentrada}")
-            print("Ejemplo PLATUNUM (1-20),GOLD (21-50),SIlVER (51-100).")
-            print("El asiento debe estar dentro de el rango de asientos del tipo de entrada")
-            while True:
-                try:
-                    asiento = int(input(f"Ingrese el asiento {i + 1}que desea comprar : "))
-                    if asiento < rangoasiento[0] or asiento > rangoasiento[1]:#rango ejemplo 1-20 ,21-50,51-100segun el tipo de entrada
+            else:
+                if tipodeentrada == 1:
+                    precio = 120000
+                    rangoasiento = (1,20) 
+                    tipoentrada = "PLATINUM"
+                elif tipodeentrada == 2:
+                    precio = 80000
+                    rangoasiento = (21,50) 
+                    tipoentrada = "GOLD"
+                elif tipodeentrada == 3:
+                    precio = 80000
+                    rangoasiento = (51,100) 
+                    tipoentrada = "SILVER"
+                else:
+                    print("El tipo de entrada no es válido")
+                    return
+            # Solicita la cantidad de entradas que desea comprar
+            cantidadentradas = int(input("Ingrese la cantidad de entradas que desea comprar (1-3) : "))
+            if cantidadentradas < 0 or cantidadentradas > 3:
+                print("La cantidad debe ser mayor que 0 y menor o igual a 3")
+                return
+            # Solicita el asiento que desea comprar
+            for i in range(cantidadentradas):
+                print("-----------------------------------------")
+                print("|              ESCENARIO                |")
+                print("-----------------------------------------")
+                print(mostrar_ubicaciones_disponibles())
+                print(f"Tipo de entrada seleccionada : {tipoentrada}")
+                print("Ejemplo PLATUNUM (1-20),GOLD (21-50),SIlVER (51-100).")
+                print("El asiento debe estar dentro de el rango de asientos del tipo de entrada ")
+                while True:
+                    try:
+                        asiento = int(input(f"Ingrese el asiento { i + 1 }que desea comprar : "))
+                        if asiento < rangoasiento[0] or asiento > rangoasiento[1]:#rango ejemplo 1-20 ,21-50,51-100segun el tipo de entrada
+                            print("El asiento no es válido  esta debe estar dentro del rango del tipo de entrada")
+                        else:
+                            break
+                    except:
                         print("El asiento no es válido")
-                    else:
-                        break
-                except:
-                    print("El asiento no es válido")
-            #registrar cliente 
-            print("-------------------------------------------------")
-            print("|          Registro de cliente                  |")
-            print("-------------------------------------------------")
-            print("Ingrese RUT del cliente")
-            rut = input("Ingrese RUT del comprador sin puntos ni guion :")
-            if "." in rut or "-" in rut:
-                print("El RUT no es válido")
-                return
-            else:
-                #registro usuario
-                registro = {
-                            "RUT": rut,
-                            "Tipo de entrada": tipoentrada,
-                            "Asiento": asiento,
-                            "Cantidad de entradas": cantidadentradas,
-                            }
-                #Marcar asiento vendido con X
-                for fila in lugaresconcierto:
-                    if asiento in fila:
-                        fila[fila.index(asiento)] = "X"
-                        break
-                #Actualizar Recaudacion
-                actualizarrecaudacionconcierto(tipoentrada,precio)
-                #Almacenar asiento comprado
-                lugarescomprados.append(registro)
-                print("Registro y Compra exitosa")
+                #registrar cliente 
                 print("-------------------------------------------------")
-                print(f"RUT: {registro['RUT']}, Tipo de entrada: {registro['Tipo de entrada']}, Asiento: {registro['Asiento']}, Cantidad de entradas: {registro['Cantidad de entradas']}\n")
+                print("|          Registro de cliente                  |")
+                print("-------------------------------------------------")
+                print("Ingrese RUT del cliente")
+                rut = input("Ingrese RUT del comprador sin puntos ni guion :")
+                if "." in rut or "-" in rut or len(rut) != 8:
+                    print("El RUT no es válido")
+                    return
+                else:
+
+                    #registro usuario
+                    registro = {
+                                "RUT": rut,
+                                "Tipo de entrada": tipoentrada,
+                                "Asiento": asiento,
+                                "Cantidad de entradas": cantidadentradas,
+                                }
+                    #Marcar asiento vendido con X
+                    for fila in lugaresconcierto:
+                        if asiento in fila:
+                            fila[fila.index(asiento)] = "X"
+                            break
+                    #Actualizar Recaudacion
+                    actualizarrecaudacionconcierto(tipoentrada,precio)
+                    #Almacenar asiento comprado
+                    lugarescomprados.append(registro)
+                    print("Registro y Compra exitosa")
+                    print("-------------------------------------------------")
+                    print(f"RUT: {registro['RUT']}, Tipo de entrada: {registro['Tipo de entrada']}, Asiento: {registro['Asiento']}, Cantidad de entradas: {registro['Cantidad de entradas']}\n")
     except ValueError:
         print("El valor ingresado no es válido")
 # Opción 2: Mostrar ubicaciones disponibles
@@ -121,6 +122,10 @@ def ver_listado_asistentes():
     #rut ordenado
     lugarescomprados.sort(key=lambda x: x['RUT'])
     with open("planilla_listar.csv", "w") as plantilla:
+        for registro in lugarescomprados:
+            plantilla.write(f"RUT: {registro['RUT']}, Tipo de entrada: {registro['Tipo de entrada']}, Asiento: {registro['Asiento']}, Cantidad de entradas: {registro['Cantidad de entradas']}\n")
+    print("Archivo 'planilla_listar.csv' creado con exito.")
+    with open("planilla_listar.json", "w") as plantilla:
         for registro in lugarescomprados:
             plantilla.write(f"RUT: {registro['RUT']}, Tipo de entrada: {registro['Tipo de entrada']}, Asiento: {registro['Asiento']}, Cantidad de entradas: {registro['Cantidad de entradas']}\n")
     print("Archivo 'planilla_listar.csv' creado con exito.")
@@ -171,4 +176,5 @@ def menu():
             else:
                 print("Opcion incorrecta")
     except ValueError:
+           print("Error.")
            print("Error.")
